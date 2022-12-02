b7c3f3f656d878d809130bb2496849a262688260
Zuzana Zahradníková <110556888+zahradnzu@users.noreply.github.com>
2022-12-02 15:24:26 +0100

    imported imput, modified

--- 2nd.py
+++ 2nd.py
@@ -1,5 +1,11 @@
+def opener(file):
+    with open(file, "r") as text_file:
+        return text_file
+
 def main():
-    pass
+    input = opener("2nd_input.txt")
+    for line in input:
+        print(line)
 
 if __name__ == "__main__":
     main()
