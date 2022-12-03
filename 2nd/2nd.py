8c6264c3fd86474072380ff0b846c0227553a75f
Zuzana Zahradníková <110556888+zahradnzu@users.noreply.github.com>
2022-12-03 15:36:37 +0100

    2nd done <3

--- 2nd/2nd.py
+++ 2nd/2nd.py
@@ -1,11 +1,28 @@
 def opener(file):
     with open(file, "r") as text_file:
-        return text_file
+        pair_list = []
+        for line in text_file:
+            pair_list.append(line.rstrip("\n"))
+        return pair_list
 
 def main():
-    input = opener("2nd_input.txt")
-    for line in input:
-        print(line)
+    print(score(opener("2nd_input.txt")))
+
+def score(l: list):
+    my_choice = 0
+    match_result = 0
+    enemy_dict = {"A":1, "B":2, "C":3}
+    ally_dict = {"X":1, "Y":2, "Z":3}
+    for pair in l:
+        my_choice += ally_dict[pair[2]]
+        if ally_dict[pair[2]] == enemy_dict[pair[0]]:
+            match_result += 3
+        elif ally_dict[pair[2]] == 1 and enemy_dict[pair[0]] == 3:
+            match_result += 6
+        elif ally_dict[pair[2]] == enemy_dict[pair[0]] + 1:
+            match_result += 6
+    score_result = my_choice + match_result
+    return score_result
 
 if __name__ == "__main__":
     main()
