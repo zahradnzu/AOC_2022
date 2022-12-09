a0bbac37c308c26a91070803bcbefdba9d54266b
Zuzana Zahradníková <zahradnikovaaaz@gmail.com>
2022-12-09 12:22:23 +0100

    nefunkcni 6

--- 6th/6th.py
+++ 6th/6th.py
@@ -9,15 +9,11 @@ def main():
     text = opener("6th_input")
     s_index = 3
     for s in text[3:]:
-        try:
-            if text[s_index] != text[(s_index - 1)] and text[s_index] != text[(s_index - 2)] and text[s_index] != text[(s_index - 3)] and text[s_index] != text[(s_index - 4)]:
-                print(s_index + 1)
-                break
-            else:
-                s_index += 1
-        except IndexError:
-            continue
-
+        if s != text[(s_index - 1)] and text[s_index] != text[(s_index - 2)] and text[s_index] != text[(s_index - 3)] and text[s_index] != text[(s_index - 4)]:
+            print(s_index + 1)
+            break
+        else:
+            s_index += 1
 
 if __name__ == "__main__":
     main()
