2e38bddb6ba433718114e458552455e924140ed7
Zuzana Zahradníková <110556888+zahradnzu@users.noreply.github.com>
2022-12-09 19:04:06 +0100

    6th done

--- 6th/6th.py
+++ 6th/6th.py
@@ -7,18 +7,16 @@ def opener(file):
 
 def main():
     text = opener("6th_input")
-    s = re.search(r"[a-z]{4}", text)
-    for string in text:
-        s = re.search(r".*[a-z]{4}", text)
-        print(s.group())
-
-
-    # for s in text:
-    #     if s != text[(s_index - 1)] and text[(s_index - 1)] != text[(s_index - 2)] and text[(s_index - 2)] != text[(s_index - 3)] and text[(s_index - 3)] != text[(s_index - 4)]:
-    #         print(text[(s_index - 4):(s_index)])
-    #         break
-    #     else:
-    #         s_index += 1
-
+    s_index = 0
+    for s in text:
+        try:
+            if s in text[(s_index + 1):(s_index + 4)] or text[(s_index + 1)] in text[(s_index + 2):(s_index + 4)] or text[(s_index + 2)] == text[(s_index + 3)]:
+                s_index += 1
+                continue
+            else:
+                print((s_index + 4))
+        except IndexError:
+            break
+        
 if __name__ == "__main__":
     main()
